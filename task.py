1b345cfb8cdd957c03b12e93b9f79756540e3824
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-05-10 11:14:48 +0400

    update

--- task.py
+++ task.py
@@ -1,12 +1,3 @@
-# salam, Hərvaxtınız xeyir.
-# Bir classdan istifadə edərək 2 metod yazın.
-# İlk metodda dəyər olaraq bir list içərisində rəqəmləri alın  və onu bir listə əlavə edin ( bu şəkildə olacaq: mylist=[1,2,3,4,5])
-# Daha sonra bir metod daha yazın. Bu metodda isə bizim bir argumentimiz olacaq (Hədəf rəqəm). Burada ilk metodda aldığımız listin
-# dəyərləri içərisində hər hansı 2 rəqəmin cəmi verilmiş hədəf rəqəmə bərabərdirsə həmin rəqəmlərin indexlərini qaytarın.
-# Əgər belə rəqəmlər yoxdursa -1 cavabı qaytarın.
-# Əlavə olaraq argumentləri hansı metodunuzda istifadə etmək istədiyiniz barəsində sərbəstsiniz və əlavə arqumentlərdən istifadə edəbilərsiniz.
-
-
 class Main:
     def __init__(self):
         self.list = []
@@ -29,8 +20,7 @@ class Main:
                 if self.list[i] + self.list[j] == self.hedef_deyer:
                     return f"indeksler: {i} ve {j} elementler"
 
-        else:
-            return -1
+        return -1
 
 
 # -------------------------------------------------------------------------------------------------
