55152f0bd3870a2f60c77490ada9c723512391ee
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-05-10 10:16:47 +0400

    commit_4

--- task.py
+++ task.py
@@ -11,11 +11,12 @@ class Main:
     def __init__(self):
         self.list = []
 
-    def liste_menimset(self):
+    def liste_menimset(self, mylist):
         # for i in range(5):
         #     self.list.append(int(input(f"{i+1} ededi daxil edin:")))
         # self.list = [1, 2, 3, 4, 5]
-        self.list = [i for i in [1, 2, 3, 4, 5]]
+        # self.list = [i for i in [1, 2, 3, 4, 5]]
+        self.list = [i for i in mylist]
 
     def list_goster(self):
         return self.list
@@ -34,6 +35,6 @@ class Main:
 
 # -------------------------------------------------------------------------------------------------
 main = Main()
-main.liste_menimset()
+main.liste_menimset([1, 2, 3, 4, 5])
 print(main.list_goster())
 print(main.hedef_deyer_yoxlama())
